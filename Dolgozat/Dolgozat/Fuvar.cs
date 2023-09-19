234f5c5a70f25e57218e5cf79455a52f838a814d
P.Laszlo <70635967+PLACKO135@users.noreply.github.com>
2023-09-19 13:06:01 +0200

    Add files via upload

---
+++ Dolgozat/Dolgozat/Fuvar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat
+{
+    internal class Fuvar
+    {
+        int taxi_id;
+        string indulas;
+        int idotartam;
+        double tavolsag;
+        double vitel_dij;
+        double borravalo;
+        string fizetes_modja;
+
+        public Fuvar(int taxi_id, string indulas, int idotartam, double tavolsag, double vitel_dij, double borravalo, string fizetes_modja)
+        {
+            this.taxi_id = taxi_id;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tavolsag = tavolsag;
+            this.vitel_dij = vitel_dij;
+            this.borravalo = borravalo;
+            this.fizetes_modja = fizetes_modja;
+        }
+
+        public int Taxi_id { get => taxi_id; set => taxi_id = value; }
+        public string Indulas { get => indulas; set => indulas = value; }
+        public int Idotartam { get => idotartam; set => idotartam = value; }
+        public double Tavolsag { get => tavolsag; set => tavolsag = value; }
+        public double Vitel_dij { get => vitel_dij; set => vitel_dij = value; }
+        public double Borravalo { get => borravalo; set => borravalo = value; }
+        public string Fizetes_modja { get => fizetes_modja; set => fizetes_modja = value; }
+    }
+}
---
+++ Dolgozat/Dolgozat/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace Dolgozat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Fuvar> Fuvar_lista = new List<Fuvar>();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            StreamReader sr = new StreamReader("fuvar.csv");
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(';');
+
+
+
+                Fuvar uj = new(int.Parse(mezok[0]), mezok[1], int.Parse(mezok[2]), double.Parse(mezok[3]), double.Parse(mezok[4]), double.Parse(mezok[5]), mezok[6]);
+            }
+            sr.Close();
+        }
+
+        private void btn_elso_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show($"3.feladat {Fuvar_lista.Count} fuvar");
+        }
+
+        private void btn_masodik_Click(object sender, RoutedEventArgs e)
+        {
+            double bevetel = 0;
+            int fuvarok_szama = 0;
+
+            foreach (Fuvar item in Fuvar_lista)
+            {
+
+                if (item.Taxi_id == 6185)
+                {
+                    bevetel += item.Vitel_dij;
+
+                    fuvarok_szama+=1;
+
+                }
+            }
+
+
+            MessageBox.Show($"4.feladat {fuvarok_szama} fuvar alatt: {bevetel}$");
+        }
+
+        private void btn_harmadik_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_negyedik_Click(object sender, RoutedEventArgs e)
+        {
+            double tavolsag = 0;
+
+            foreach (Fuvar item in Fuvar_lista)
+            {
+                tavolsag += item.Tavolsag * 1.6;
+            }
+
+            MessageBox.Show($"6.feladat{Math.Round(tavolsag, 2)}km");
+        }
+
+       private void btn_ötödik_Click(object sender, RoutedEventArgs e)
+       {
+            List<Fuvar> Hibak = new List<Fuvar>();
+
+            StreamWriter sr = new StreamWriter("hibak.txt");
+            sr.WriteLine("taxi_id,indulas,idotartam,tavolsag,vitel_dij,borravalo,fizetes_modja");
+
+            for (int i = 0; i < Hibak.Count; i++)
+            {
+
+            }
+
+       }
+       }
+    }
